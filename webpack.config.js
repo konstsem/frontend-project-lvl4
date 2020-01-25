0f18320fb1c7e025526d8923b872900d80fab0b5
konstsem <konstantin.semenov.2@gmail.com>
2020-01-25 23:54:52 +0300

    fix eslint errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const isProduction = process.env.NODE_ENV === 'production';
 const isDevelopment = !isProduction;
 
