08ca2ffac4155ae5699cf4bac82073650e45cd35
konstsem <konstantin.semenov.2@gmail.com>
2020-03-03 17:38:19 +0300

    feat: add sending new message to server

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { connect } from 'react-redux';
 import {
   Formik,
   Form,
   Field,
   ErrorMessage,
 } from 'formik';
+import axios from 'axios';
+import UserNameContext from '../../context';
+import routes from '../../routes';
 
-const NewMessageForm = () => (
-  <Formik
-    initialValues={{ message: '' }}
-    onSubmit={(values, { setSubmitting }) => {
-      setTimeout(() => {
-        // console.log(values.message);
-        setSubmitting(false);
-      }, 500);
-    }}
-  >
-    {({ isSubmitting }) => (
-      <Form className="form">
-        <Field className="container-fluid" type="text" name="message" />
-        <button className="collapse" type="submit" disabled={isSubmitting}>
-          Submit
-        </button>
-        <ErrorMessage name="message" component="div" />
-      </Form>
-    )}
-  </Formik>
-);
+const mapStateToProps = (state) => {
+  const props = {
+    currentChannelId: state.currentChannelId,
+  };
+  return props;
+};
 
-export default NewMessageForm;
+const NewMessageForm = ({ currentChannelId }) => {
+  const userName = useContext(UserNameContext);
+  const messagesPath = routes.channelMessagesPath(currentChannelId);
+  return (
+    <Formik
+      initialValues={{ message: '' }}
+      onSubmit={({ message }, { setSubmitting, resetForm }) => {
+        setTimeout(() => {
+          setSubmitting(false);
+        }, 5000);
+        // send message to server
+        axios.post(messagesPath, { data: { attributes: { author: userName, text: message } } })
+          .then(resetForm())
+          .catch((err) => console.error(err));
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form className="form">
+          <Field className="container-fluid" type="text" name="message" />
+          <button className="collapse" type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+          <ErrorMessage name="message" component="div" />
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default connect(mapStateToProps)(NewMessageForm);
